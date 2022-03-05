c850c0eb17887cbf9e4b863d6a7c28a4f1f6b77a
naomimwangi <naomimwangi7@gmail.com>
2022-03-05 13:15:51 +0300

    add variables and functions

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 //setting variables
 let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-let days = ["Sundaay", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
 
-d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+//reference formula: d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
 
-function getakanname() {
 //html linking
-  let gender = document.getElementsByClassName('gender')
-  let date = document.getElementsByClassName('date')
-  let month = document.getElementsByClassName('month')
-  let year = document.getElementsByClassName('year')
+let genders = document.getElementsByClassName('gender')
+let date = document.getElementsByClassName('date').value
+let month = document.getElementsByClassName('month').value
+let year = document.getElementsByClassName('year').value
+
+//calculating day value
+function calculateDayValue() {
+    CC = parseInt(year.substring(0, 2));
+    YY = parseInt(year.substring(2, 4));
+    MM = parseInt(month);
+    DD = parseInt(date);
+    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+    console.log (d);
+    return (math.floor(d));
 }
 
 
