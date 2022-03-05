7369e7feb158d3fb63d2014a3b7612d0ed23b689
naomimwangi <naomimwangi7@gmail.com>
2022-03-05 12:06:30 +0300

    link html elements and set variables

---
+++ index.js
@@ -0,0 +1,20 @@
+//setting variables
+let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let yy = 
+
+d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7
+
+function getakanname() {
+//html linking
+  let gender = document.getElementsByClassName('gender')
+  let date = document.getElementsByClassName('date')
+  let month = document.getElementsByClassName('month')
+  let year = document.getElementsByClassName('year')
+}
+
+
+
+
+
+
